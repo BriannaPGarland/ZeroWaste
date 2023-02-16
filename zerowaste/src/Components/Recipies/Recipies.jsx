970909d296b655694488d599f7fd303547aec1a7
ShivankSrivastava <74642182+ShivankSrivastava@users.noreply.github.com>
2023-02-15 20:02:08 -0500

    Created separate folders for each component

---
+++ zerowaste/src/Components/Recipies/Recipies.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Recipies = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Recipies
---
+++ zerowaste/src/Components/index.js
@@ -0,0 +1,6 @@
+export { default as Analytics } from './Analytics.jsx';
+export { default as Donation } from './Donations.jsx';
+export { default as Home } from './Homes.jsx';
+export { default as Login } from './Login.jsx';
+export { default as Navbar } from './Navbar.jsx';
+export { default as Recipies } from './Recipies.jsx';

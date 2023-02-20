5ea6b3dfc75a8cdf9299a1c47b88e68f0b551167
Brianna Garland <56358388+BriannaPGarland@users.noreply.github.com>
2023-02-20 16:58:12 -0500

    Adding Sprint 1 Updates

--- zerowaste/src/Components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Analytics } from './Analytics/Analytics.jsx';
-export { default as Donations } from './Donations/Donations.jsx';
-export { default as Home } from './Home/Home.jsx';
-export { default as Login } from './Login/Login.jsx';
-export { default as Signup } from './Login/Signup.jsx';
-export { default as Navbar } from './Navbar/Navbar.jsx';
-export { default as Recipies } from './Recipies/Recipies.jsx';

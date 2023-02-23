4cce7a1d6343ffbdbb9513535e59119e8c04dbc8
ShivankSrivastava <74642182+ShivankSrivastava@users.noreply.github.com>
2023-02-22 20:12:41 -0500

    CSS

--- zerowaste/src/Components/Home/Home.jsx
+++ zerowaste/src/Components/Home/Home.jsx
@@ -39,7 +39,7 @@ const Home = () => {
 				<div >
 					<div className = "searchSection">
 						<input className ="searchbar" type="text" placeholder="Search ..."></input>
-						<img className="searIcon" src={searchIcon}></img>
+						<img className="searIcon" src={searchIcon} alt=''></img>
 					</div>
 				</div>
 			</section>

99d24789e92d6e4fa443608481d50fa69536ddf0
Femi <femirocks123@gmail.com>
2023-03-01 18:47:53 -0500

    Finish RestaurantDB_API
        getRestaurant, // works
        insertRestaurant, //works
        deleteRestaurant, //works
        updateRestaurant, //works
    
        getIngredients, //works
        updateIngredients,//works
        insertIngredients, //works
        deleteIngredients, //works
    
        getRecipes, //works
        updateRecipes,//works
        insertRecipes,//works
        deleteRecipes//works

--- zerowaste/src/scripts/mongoConnect.js
+++ zerowaste/src/scripts/mongoConnect.js
@@ -13,27 +13,31 @@ const url = 'mongodb://0.0.0.0:27017/';
 MongoClient.connect(url).then((client) => {      
     // database name
     const db =  client.db("ZeroWasteDB");
-    populateDB(db,"Restaurants")
+    testDB("Restaurants")
 })
 
 
-let populateDB = async function(db,db_type){ //Test items to fill database
+let testDB = async function(db_type){ //Test items to fill database
     //console.log(db,db_type);
     if (db_type == "Restaurants"){
-        restaurant_db.Helper("Insert", {
+        restaurant_db.insertRestaurant({
             "contact_info": {"owner": "John Smith" , "phone": "000-000-0000","email": "test@test.com",},
             "address": {"street": "", "city": "", "state": "", "zip": "", "country":""},
-            "ingredients": ["Test"],
-            "recipes": ["one"],
+            "ingredients": [{"_id" : new ObjectId(), "data": {"name": "TestORANGE", "amount":20}}],
+            "recipes": [{"_id" : new ObjectId(), "data": {"name": "TestPIE", "amount":2}}],
             "money_saved": 12000,
             "is_food_claimed": false,
         })
        // restaurant_db("Delete",{_id: new ObjectId("63fc343bfc34ed0474a4f19a")} )
         //let result =  await restaurant_db.getIngredients(new ObjectId("63fe546d4e7664aef7f40a5e"));
-        let obj = { "_id": new ObjectId("63fe546d4e7664aef7f40a5e")}
+        let obj = { "_id": new ObjectId("63ffda769c9384f917cbc006")}
         //dlet result =  await restaurant_db.getRecipes();
+        let newIngredient = {"_id" : new ObjectId(), "data": {"name": "TestORANGE", "amount": 10, "Expiration": new Date()}}
+        let updateingredient = {"name": "TestORANGE", "amount": 10, "Expiration": new Date()}
+        let newRecipe = {"_id" : new ObjectId(), "data": {"name": "WHATTHEFUCK","amount":200}}
+        let updateRecipe = {"name": "WHATTHEFUCK","amount":100, "Surplus": 20}
 
-        console.log(result);
+        console.log(await restaurant_db.deleteRecipes());
     }
     else if(db_type == "Recipes")
         db.collection(db_type).insertOne(

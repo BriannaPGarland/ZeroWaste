ac946566e43fe754796d8b40ddf3f8e1aaf4afdd
Femi <femirocks123@gmail.com>
2023-03-01 18:48:36 -0500

    Update mongoConnect.js

--- zerowaste/src/scripts/mongoConnect.js
+++ zerowaste/src/scripts/mongoConnect.js
@@ -30,14 +30,14 @@ let testDB = async function(db_type){ //Test items to fill database
         })
        // restaurant_db("Delete",{_id: new ObjectId("63fc343bfc34ed0474a4f19a")} )
         //let result =  await restaurant_db.getIngredients(new ObjectId("63fe546d4e7664aef7f40a5e"));
-        let obj = { "_id": new ObjectId("63ffda769c9384f917cbc006")}
+        //let obj = { "_id": new ObjectId("63ffda769c9384f917cbc006")}
         //dlet result =  await restaurant_db.getRecipes();
-        let newIngredient = {"_id" : new ObjectId(), "data": {"name": "TestORANGE", "amount": 10, "Expiration": new Date()}}
-        let updateingredient = {"name": "TestORANGE", "amount": 10, "Expiration": new Date()}
-        let newRecipe = {"_id" : new ObjectId(), "data": {"name": "WHATTHEFUCK","amount":200}}
-        let updateRecipe = {"name": "WHATTHEFUCK","amount":100, "Surplus": 20}
+        // newIngredient = {"_id" : new ObjectId(), "data": {"name": "TestORANGE", "amount": 10, "Expiration": new Date()}}
+        //let updateingredient = {"name": "TestORANGE", "amount": 10, "Expiration": new Date()}
+        //let newRecipe = {"_id" : new ObjectId(), "data": {"name": "WHATTHEFUCK","amount":200}}
+        //let updateRecipe = {"name": "Test","amount":100, "Surplus": 20}
 
-        console.log(await restaurant_db.deleteRecipes());
+        //console.log(await restaurant_db.deleteRecipes());
     }
     else if(db_type == "Recipes")
         db.collection(db_type).insertOne(

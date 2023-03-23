2f7d242a1fd106534d01724bb8b97ebc208926d3
ShivankSrivastava <74642182+ShivankSrivastava@users.noreply.github.com>
2023-03-23 17:25:16 -0400

    .

--- zerowaste/src/Components/LandingPage/LandingPage.jsx
+++ zerowaste/src/Components/LandingPage/LandingPage.jsx
@@ -6,29 +6,9 @@ import CarouselComponent from './CarouselComponent';
 
 const Landing = () => {
   return (
-<<<<<<< HEAD
     <div className="landing">
     <div style={{height:"15em"}}>
       < CarouselComponent/>
-=======
-    <div class="landing">
-        <h1 class="title">
-            Welcome!
-        </h1>
-        <p class="Landtxt">to</p>
-        <div className="logoImg"></div>
-        <div className="buttonSec">
-            <Link className="SignUpButt" to="/Signup">
-            Sign Up For Free 
-            </Link>
-        </div>
-        <div className="buttonSec">
-                <Link className="LGINButt" to="/Login">
-               Login
-                </Link>
-        </div>
-      
->>>>>>> eb777ea5dbf272db65607b858ca75071d3752790
     </div>
     <div className="landing-content">
       <h1 className="title">Welcome!</h1>

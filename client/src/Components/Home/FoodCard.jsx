0210dc3a2788cf9f8781e025e2e5c98e4ed83a74
Siddhant Kumar <siddhantkumar052@gmail.com>
2023-03-29 21:21:55 -0400

    fix: pages: food card, home

--- client/src/Components/Home/FoodCard.jsx
+++ client/src/Components/Home/FoodCard.jsx
@@ -1,19 +1,14 @@
+import "./Home.css";
 
-import './Home.css';
+import React, { Component } from "react";
 
-import React, { Component } from 'react';
- 
+export default function FoodCard(props) {
+  const { name, exp, amount, units } = props;
 
-export default function FoodCard (props) {
-  const { name,exp,amount,units } = props;
-
-  
-
-    return (
-      <div className="foodcard">
-          <div className="foodname">
-<<<<<<< HEAD:zerowaste/src/Components/Home/FoodCard.jsx
-                {name}
+  return (
+    <div className="foodcard">
+      <div className="foodname">
+        {/* {name}
           </div>
           <hr></hr>
           <div className="foodamount">
@@ -21,20 +16,13 @@ export default function FoodCard (props) {
           </div>
           <hr></hr>
           <div className="foodamount">
-                Expires: {exp}
-=======
-              Name: Sugar
-          </div>
-          <hr></hr>
-          <div className="foodamount">
-                Amount        2lbs
-          </div>
-          <hr></hr>
-          <div className="foodamount">
-                Expires: 04/24/2024
->>>>>>> origin/sidd:client/src/Components/Home/FoodCard.jsx
-          </div>
+                Expires: {exp} */}
+        Name: Sugar
       </div>
-    );
-  
+      <hr></hr>
+      <div className="foodamount">Amount 2lbs</div>
+      <hr></hr>
+      <div className="foodamount">Expires: 04/24/2024</div>
+    </div>
+  );
 }

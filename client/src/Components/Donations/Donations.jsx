aa7d6915f32e22431434bc04be0c45f9d6377339
Brianna Garland <56358388+BriannaPGarland@users.noreply.github.com>
2023-04-03 18:31:50 -0400

    Updates

--- client/src/Components/Donations/Donations.jsx
+++ client/src/Components/Donations/Donations.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./Donate.css";
 import MapComponent from "./Map.js"
-import googleMapStyles from "./GoogleMapStyle.js"
+import shelterItem from "./shelterListItem.js";
 const Donations = () => {
 
 
@@ -12,6 +12,17 @@ const Donations = () => {
       <h1 class="title">
             Donations
       </h1>
+      <div className="leftColumn">
+        <div className="DonTitle">
+            Communities in Need
+        </div> 
+        <div className="underline">
+        </div>
+        <div className="listed">
+          <shelterItem></shelterItem>
+        </div>
+          
+        </div>
       <div className="mapzone">
         <MapComponent></MapComponent>
       </div>
---
+++ client/src/Components/Donations/shelterListItem.js
@@ -0,0 +1,13 @@
+import "./Donate.css";
+
+import React, { Component } from "react";
+
+export default function shelterListItem(props) {
+  const { name, exp, amount, units } = props;
+
+  return (
+   <div>
+       shelter
+   </div>
+  );
+}

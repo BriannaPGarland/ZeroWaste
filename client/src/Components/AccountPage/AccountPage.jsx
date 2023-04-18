718078a967c05e6af73447b9b92bdf731c9f7e6a
Brianna Garland <56358388+BriannaPGarland@users.noreply.github.com>
2023-04-18 00:19:38 -0400

    Adding settings and account page as well as fixing css

--- client/src/Components/AccountPage/AccountPage.jsx
+++ client/src/Components/AccountPage/AccountPage.jsx
@@ -1,17 +1,32 @@
 import React from "react";
 import "./AccountPage.css";
 const AccountPage = () => {
+  const nameAcc="Brianna Garland"
+  const emailAcc="bgarland@stevens.edu"
+  const numAcc="908-343-4281"
+  const locAcc="Hoboken,NJ"
+  const resAcc="Applebees"
+  const numDonAcc="180"
   return (
     <div className="accountPage">
       <h1 class="title">Account</h1>
       <div className="Content">
-        UserName: Brianna Garland
-      </div>
-      <div className="Content">
-        Restaurant: Applebees
-      </div>
-      <div className="Content">
-        Number of Times Donated: 150
+       <div className="leftAcc">
+          <div className="accItem">Name:</div>
+          <div className="accItem">Email:</div>
+          <div className="accItem">Phone Number:</div>
+          <div className="accItem">Location:</div>
+          <div className="accItem">Restaurant Name:</div>
+          <div className="accItem">Number of Times Donated:</div>
+       </div>
+       <div className="rightAcc">
+        <input className="accIN" value={nameAcc} ></input>
+        <input className="accIN" type="email" value={emailAcc}></input>
+        <input className="accIN" type="tel" value={numAcc}></input>
+        <input className="accIN" value={locAcc}></input>
+        <input className="accIN" value={resAcc}></input>
+        <input className="accIN" type ="number" value ={numDonAcc}></input>
+       </div>
       </div>
     </div>
   );

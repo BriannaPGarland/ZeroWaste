ab1379428656d867d2c73f4052dba7eb60784308
Siddhant Kumar <siddhantkumar052@gmail.com>
2023-04-25 20:23:58 -0400

    feat : account page

--- client/src/Components/SettPage/SettPage.jsx
+++ client/src/Components/SettPage/SettPage.jsx
@@ -1,24 +1,22 @@
-import React, { useContext,useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AuthorizeContext } from "../../Authorization/Authorize";
 import { auth } from "../../Authorization/FirebaseConfig";
 import Landing from "../LandingPage/LandingPage";
-import './SettPage.css'
+import "./SettPage.css";
 import { useNavigate } from "react-router-dom";
 
 
 const SettingPage = () => {
-
+  
   const { user } = useContext(AuthorizeContext);
-const navigate =  useNavigate();
-useEffect(()=>{
-  if (!user) {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!user) {
+      navigate("/");
+    }
+  }, []);
 
-    navigate("/")
 
-  }
-
-
-}, [])
 
   return (
     <div className="SettingsPage">
@@ -35,14 +33,13 @@ useEffect(()=>{
               <input type="checkbox"></input>
               <span class="slider round"></span>
             </label>
-           
+
             <label class="switch">
               <input type="checkbox"></input>
               <span class="slider round"></span>
             </label>
           </div>
         </div>
-        
       </div>
       <div className="SetContent">
         Inventory
@@ -52,8 +49,8 @@ useEffect(()=>{
             <div className="item">Days to be notified prior 2 expiration:</div>
           </div>
           <div className="rightNot">
-              <input type="number"className="settIn"value="1"></input>
-              <input type="number"className="settIn"value="2"></input>
+            <input type="number" className="settIn" value="1"></input>
+            <input type="number" className="settIn" value="2"></input>
           </div>
         </div>
       </div>
